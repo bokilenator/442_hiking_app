a772971c4249fbf1c5b54253fbaddd1862ff1991
Karan Bokil <bokilenator@gmail.com>
2018-11-01 13:01:18 -0400

    Added trail index view + mapbox map for trail

---
+++ outback/Models/Trail.swift
@@ -0,0 +1,22 @@
+//
+//  Trail.swift
+//  outback
+//
+//  Created by Karan Bokil on 11/1/18.
+//  Copyright © 2018 Karan Bokil. All rights reserved.
+//
+
+import Foundation
+struct Trail {
+  let name: String
+  let summary: String
+  let difficulty: String
+  let rating: Float
+  let url: String
+  let img: String
+  let length: Int
+  let longitude: Float
+  let latitude: Float
+  let condition: String
+  let condition_details: String
+}
---
+++ outback/Trail Detail/TrailDetailViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  TrailDetailViewModel.swift
+//  outback
+//
+//  Created by Karan Bokil on 11/1/18.
+//  Copyright © 2018 Karan Bokil. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+
+class TrailDetailViewModel {
+  let trail: Trail
+  
+  init(trail: Trail) {
+    self.trail = trail
+  }
+  
+  func title() -> String {
+    return trail.name
+  }
+
+  
+  func startCoordinate() -> CLLocationCoordinate2D {
+    var startCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(trail.latitude), longitude: CLLocationDegrees(trail.longitude))
+    return startCoordinate
+  }
+
+}
