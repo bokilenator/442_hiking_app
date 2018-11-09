6e254afa907bd74a5483a6dcd07e54df0e21d77e
Karan Bokil <bokilenator@gmail.com>
2018-11-09 13:36:46 -0500

    tested TrailDetailsViewModel

---
+++ outbackTests/MapViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewModelTests.swift
+//  outbackTests
+//
+//  Created by Karan Bokil on 11/9/18.
+//  Copyright © 2018 Karan Bokil. All rights reserved.
+//
+
+import XCTest
+@testable import outback
+
+class MapViewModelTests: XCTestCase {
+  
+}
---
+++ outbackTests/ParksViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  ParksViewModelTests.swift
+//  outbackTests
+//
+//  Created by Karan Bokil on 11/9/18.
+//  Copyright © 2018 Karan Bokil. All rights reserved.
+//
+
+import XCTest
+@testable import outback
+
+class ParksViewModelTests: XCTestCase {
+  
+}
